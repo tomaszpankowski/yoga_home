43b2c7ada27474f969b8a3ea87f7ff9a05831892
tomaszpankowski <pankowski.email@gmail.com>
2022-01-21 12:59:38 +0100

    modified routing

--- src/config.js
+++ src/config.js
@@ -1,16 +1,26 @@
 import {Redirect} from "react-router-dom";
+import About from "./views/about";
+import Blog from "./views/blog";
 import Comingsoon from "./views/comingsoon";
+import Community from "./views/community";
 import Contact from "./views/contact";
 import Error from "./views/error";
+import Home from "./views/home";
 import Login from "./views/login";
 import PrivacyPolicy from "./views/privacy";
 import Register from "./views/register";
+import Reviews from "./views/reviews";
 
 const appCfg = {
     routes:[
-        {navItem:true, exact:true, path:"/",name:"Comingsoon",view:<Comingsoon/>},
+        {navItem:true, exact:true, path:"/",name:"Home",view:<Home/>},
+        {navItem:true, exact:true, path:"/about",name:"About",view:<About/>},
+        {navItem:true, exact:true, path:"/community",name:"Community",view:<Community/>},
+        {navItem:true, exact:true, path:"/blog",name:"Blog",view:<Blog/>},
+        {navItem:true, exact:true, path:"/reviews",name:"Reviews",view:<Reviews/>},
         {navItem:true, exact:true, path:"/contact",name:"Contact",view:<Contact/>},
         {navItem:true, exact:true, path:"/login",name:<span className="fa fa-user-circle"></span>,view:<Login backLink={{name:" Register now!",href:"/register"}}/>},
+        {navItem:false, exact:true, path:"/comingsoon",name:"Comingsoon",view:<Comingsoon/>},
         {navItem:false, exact:true, path:"/register",name:"Register",view:<Register backLink={{name:" Login now!",href:"/login"}}/>},
         {navItem:false, exact:false, path:"/error",name:"Error",view:<Error backLink={{name:"back to Home",href:"/"}}/>},
         {navItem:false, exact:true, path:"/privacy",name:"Privacy",view: <PrivacyPolicy backLink={{name:"back to Home",href:"/"}}/>},
---
+++ src/views/home.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Home extends Component{
+    render(){
+        return(        
+            <Container fluid className="home-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Home;
